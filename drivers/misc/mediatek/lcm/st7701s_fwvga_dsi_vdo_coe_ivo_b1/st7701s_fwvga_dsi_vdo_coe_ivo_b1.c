c0d74d99f05a4fdaa29c6888f32a353dbe7d868b
DevMelek <56092115+DevMelek@users.noreply.github.com>
2020-03-18 14:22:16 +0100

    Fix

--- drivers/misc/mediatek/lcm/st7701s_fwvga_dsi_vdo_coe_ivo_b1/st7701s_fwvga_dsi_vdo_coe_ivo_b1.c
+++ drivers/misc/mediatek/lcm/st7701s_fwvga_dsi_vdo_coe_ivo_b1/st7701s_fwvga_dsi_vdo_coe_ivo_b1.c
@@ -1,4 +1,4 @@
-
+ 
 /*----------------------------------------------------------------
 * Author : Rubén Espínola (ruben1863@github.com)
 * Contact : rubenes2003@gmail.com
@@ -6,40 +6,40 @@
 * Reversed for Melek Saidani
 * Copyright 2019 © Rubén Espínola
  *---------------------------------------------------------------*/
-
-
+ 
+#include <mt-plat/mt_gpio.h>
 #include "lcm_drv.h"
-
-
+ 
+ 
 // ---------------------------------------------------------------------------
 //  Local Constants
 // ---------------------------------------------------------------------------
-
-
-
+ 
+ 
+ 
 #define FRAME_WIDTH                                         (960)
 #define FRAME_HEIGHT                                        (480)
-
-
+ 
+ 
 #define REGFLAG_DELAY               (0XFE)
 #define REGFLAG_END_OF_TABLE        (0x00)
-
-
+ 
+ 
 // ---------------------------------------------------------------------------
 //  Local Variables
 // ---------------------------------------------------------------------------
-
+ 
 static LCM_UTIL_FUNCS lcm_util = {0};
-
+ 
 #define SET_RESET_PIN(v) (lcm_util.set_reset_pin((v)))
 #define UDELAY(n) (lcm_util.udelay(n))
 #define MDELAY(n) (lcm_util.mdelay(n))
-
-
+ 
+ 
 // ---------------------------------------------------------------------------
 //  Local Functions
 // ---------------------------------------------------------------------------
-
+ 
 #define dsi_set_cmdq_V3(para_tbl,size,force_update)         lcm_util.dsi_set_cmdq_V3(para_tbl,size,force_update)
 #define dsi_set_cmdq_V2(cmd, count, ppara, force_update)	lcm_util.dsi_set_cmdq_V2(cmd, count, ppara, force_update)
 #define dsi_set_cmdq(pdata, queue_size, force_update)		lcm_util.dsi_set_cmdq(pdata, queue_size, force_update)
@@ -47,18 +47,18 @@ static LCM_UTIL_FUNCS lcm_util = {0};
 #define write_regs(addr, pdata, byte_nums)	                lcm_util.dsi_write_regs(addr, pdata, byte_nums)
 #define read_reg(cmd)   lcm_util.dsi_dcs_read_lcm_reg(cmd)
 #define wrtie_cmd(cmd)	lcm_util.dsi_write_cmd(cmd)
-
+ 
  struct LCM_setting_table {
     unsigned cmd;
     unsigned char count;
     unsigned char para_list[64];
 };
-
-
+ 
+ 
 static struct LCM_setting_table lcm_initialization_setting[] = {
-
+ 
 };	
-
+ 
 static struct LCM_setting_table lcm_deep_sleep_mode_in_setting[] = 
 {
     { 0x28, 0x01, {0x00}},
@@ -67,11 +67,11 @@ static struct LCM_setting_table lcm_deep_sleep_mode_in_setting[] =
     { REGFLAG_DELAY, 120, {0x00}},
     { REGFLAG_END_OF_TABLE, 0x00, {0x00}}
 };
-
+ 
 static void push_table(struct LCM_setting_table *table, unsigned int count, unsigned char force_update)
 {
     unsigned int i;
-
+ 
     for(i = 0; i < count; i++) {
         
         unsigned cmd;
@@ -91,22 +91,22 @@ static void push_table(struct LCM_setting_table *table, unsigned int count, unsi
     }
     
 }
-
-
+ 
+ 
 // ---------------------------------------------------------------------------
 //  LCM Driver Implementations
 // ---------------------------------------------------------------------------
-
+ 
 static void lcm_set_util_funcs(const LCM_UTIL_FUNCS *util)
 {
     memcpy(&lcm_util, util, sizeof(LCM_UTIL_FUNCS));
 }
-
-
+ 
+ 
 static void lcm_get_params(LCM_PARAMS *params)
 {
 	memset(params, 0, sizeof(LCM_PARAMS));	
-
+ 
 	params->physical_width = 61;
 	params->physical_height = 123;
 	params->physical_width_um = 61880;
@@ -114,7 +114,7 @@ static void lcm_get_params(LCM_PARAMS *params)
 	params->width = 480;
 	params->height = 960;
 	params->physical_height_um = 123750;
-	params->dsi.lane_swap_en = 6;
+//	params->dsi.lane_swap_en = 6;
 	params->dsi.vertical_active_line = 960;
 	params->dsi.data_format.color_order = 0;
 	params->dsi.data_format.trans_seq = 0;
@@ -144,45 +144,45 @@ static void lcm_get_params(LCM_PARAMS *params)
 	params->dsi.noncont_clock = 1;
 	params->dsi.noncont_clock_period = 2;
 }
-
+ 
 static void lcm_init(void)
 {
-	mt_set_gpio_out(0x80000046, GPIO_OUT_1);
+	mt_set_gpio_out(0x80000046, GPIO_OUT_ONE);
 	MDELAY(5);
-	mt_set_gpio_out(0x80000046, GPIO_OUT_0);
+	mt_set_gpio_out(0x80000046, GPIO_OUT_ZERO);
 	MDELAY(10);
-	mt_set_gpio_out(0x80000046, GPIO_OUT_1);
+	mt_set_gpio_out(0x80000046, GPIO_OUT_ONE);
 	MDELAY(120);
-
+ 
 	push_table(lcm_initialization_setting, sizeof(lcm_initialization_setting) / sizeof(struct LCM_setting_table), 1);
 }
-
-
+ 
+ 
 static void lcm_suspend(void)
 {
 	push_table(lcm_deep_sleep_mode_in_setting, sizeof(lcm_deep_sleep_mode_in_setting) / sizeof(struct LCM_setting_table), 1);
-
-	mt_set_gpio_out(0x80000046, GPIO_OUT_0);
+ 
+	mt_set_gpio_out(0x80000046, GPIO_OUT_ZERO);
 }
-
+ 
 static void lcm_resume(void)
 {
-	mt_set_gpio_out(0x80000046, GPIO_OUT_1);
+	mt_set_gpio_out(0x80000046, GPIO_OUT_ONE);
 	MDELAY(5);
-	mt_set_gpio_out(0x80000046, GPIO_OUT_0);
+	mt_set_gpio_out(0x80000046, GPIO_OUT_ZERO);
 	MDELAY(10);
-	mt_set_gpio_out(0x80000046, GPIO_OUT_1);
+	mt_set_gpio_out(0x80000046, GPIO_OUT_ONE);
 	MDELAY(10);
-
+ 
 	push_table(lcm_initialization_setting, sizeof(lcm_initialization_setting) / sizeof(struct LCM_setting_table), 1);
 }
-
+ 
 static unsigned int lcm_compare_id(void)
 {
 	return 1;
 }
-
-
+ 
+ 
 // ---------------------------------------------------------------------------
 //  Get LCM Driver Hooks
 // ---------------------------------------------------------------------------
@@ -195,6 +195,7 @@ LCM_DRIVER st7701s_fwvga_dsi_vdo_coe_ivo_b1_lcm_drv =
     .suspend        = lcm_suspend,
     .resume         = lcm_resume,   
     .compare_id     = lcm_compare_id,
-
+ 
 };
-
+ 
+ 
