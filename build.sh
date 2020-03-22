6c336413cbdc82e8cba9e33f6fba38bbf1e2dc4b
DevMelek <56092115+DevMelek@users.noreply.github.com>
2020-03-22 13:48:51 +0100

    T

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export KBUILD_BUILD_USER=melek
 export KBUILD_BUILD_HOST=TRY
-export CROSS_COMPILE=${PWD}/gcc/bin/arm-eabi
+export CROSS_COMPILE=${PWD}/arm-linux-gnueabihf-linaro-6.3.1-mt6580/bin/arm-linux-gnueabihf
 export USE_CCACHE=1
 export ARCH=arm ARCH_MTK_PLATFORM=mt6580
 export TARGET=out
-make O=$TARGET ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE w370_defconfig
+make O=$TARGET ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE V060C6_defconfig
 make O=$TARGET ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE -j4
 
